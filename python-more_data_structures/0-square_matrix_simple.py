f15a8639c794fc8cb704a954f75090ff0d53a964
Cassik3033 <cassinewman21@gmail.com>
2023-09-12 18:55:11 -0700

    0-2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
+    square = []
     for line in matrix:
-        squared.append([c**2 for c in line])
-        return squared
+        square.append([c**2 for c in line])
+        return square
